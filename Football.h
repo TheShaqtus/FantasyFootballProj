538053b61c7c0cc130acd64d229e3b8c713b1f22
TheShaqtus <Adam.Mraz@colorado.edu>
2015-04-22 17:17:11 -0600

    Updated structure of class.

---
+++ Football.h
@@ -0,0 +1,42 @@
+#ifndef FOOTBALL_H
+#define FOOTBALL_H
+
+struct node{
+	int games;
+	std::string name;
+	std::string team;
+	std::string position;
+	int points;
+	float avgPoints;
+	node* parent;
+	node* right;
+	node* left;
+	
+	node(){};
+	
+	node(std::string in_name, std::string in_team, int in_games, std::string in_position, int in_points){
+		name = in_name;
+		team = in_team;
+		games = in_games;
+		position = in_position;
+		points = in_points;
+		avgPoints = points/games;
+		left == NULL;
+		right == NULL;
+		parent = NULL;
+
+};
+
+class StatTree{
+	
+	public:
+		StatTree();
+		~StatTree();
+		void addPlayer(std::string, std::string, int, std::string, int);
+		void printTree();
+		void printByPosition(std::string);
+		void comparePlayers(std::string, std::string);
+		
+
+
+#endif
