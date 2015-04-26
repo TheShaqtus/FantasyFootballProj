cbe7c3388d4350908094e0062d6606f3d70153d0
TheShaqtus <Adam.Mraz@colorado.edu>
2015-04-26 16:49:54 -0600

    Added Compare Player and Print Database functions

--- Football.cpp
+++ Football.cpp
@@ -27,14 +27,56 @@ Database::~Database()
 }
 
 void Database::buildDatabase(char* fileName){
-	
-	
+
+
 }
 
 void Database::printDatabase(){
+    player* curr = root;
+    cout << "Name   Team   Position   Games Played   Points   Points/Game" << endl;
+    while(curr != NULL){
+        cout << curr->name << "  " << curr->team << "  " << curr->position << "  " << curr->games << "  " << curr->points << "  " << curr->avgPoints << endl;
+        curr = curr->next;
+    }
 }
 
 void Database::comparePlayers(string name1, string name2){
+    player* curr = root;
+    int count = 2;
+    player* player1;
+    player* player2;
+    int gamesDel = 0;
+    int pointsDel = 0;
+    float avgPointsDel = 0;
+
+    while(curr != NULL){
+        if(curr->name.compare(name1) == 0){
+
+            count--;
+            player1 = curr;
+        }else if(curr->name.compare(name2) == 0){
+
+            count--;
+            player2 = curr;
+        }
+        if(count == 0){
+            break;
+        }
+        curr = curr->next;
+    }
+    if(count != 0){
+        cout << "Player(s) not found." << endl;
+        return;
+    }
+    cout << "Name   Team   Position   Games Played   Points   Points/Game" << endl;
+    cout << player1->name << "  " << player1->team << "  " << player1->position << "  " << player1->games << "  " << player1->points << "  " << player1->avgPoints << endl;
+    cout << player2->name << "  " << player2->team << "  " << player2->position << "  " << player2->games << "  " << player2->points << "  " << player2->avgPoints << endl;
+    gamesDel = player1->games - player2->games;
+    pointsDel = player1->points - player2->points;
+    avgPointsDel = player1->avgPoints - player2->avgPoints;
+    cout << "Games Played Difference: " << gamesDel << endl;
+    cout << "Fantasy Points Difference: " << pointsDel << endl;
+    cout << "Average Fantasy Points Difference: " << avgPointsDel << endl;
 }
 
 void Database::compareTeams(string name1, string name2){
